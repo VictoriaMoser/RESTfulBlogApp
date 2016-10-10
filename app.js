bcc0425e0532ab3adf701c51a1bf883003aa0ce6
Victoria Moser <moservictoria02@gmail.com>
2016-10-09 20:56:18 -0400

    initial commit and added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var bodyParser = require ("body-parser"),
+mongoose       = require ("mongoose"),
+express        = require ("express"),
+app            = express();
+
+
+// Fields of out app
+// title
+// image
+// body
+// created
