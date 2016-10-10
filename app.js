0cc190aaa1a1b07d77827f071798086c6102b429
Victoria Moser <moservictoria02@gmail.com>
2016-10-09 22:11:15 -0400

    fixing the app.js file and adding ejs node_modules

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ mongoose       = require ("mongoose"),
 express        = require ("express"),
 app            = express();
 
-
+mongoose.connect("mongodb://localhost/restful_blog_app");
+//next code can use in other apps:
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 // Fields of out app
 // title
 // image
 // body
 // created
+
+//to make sure our app is working:
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("SERVER IS RUNNING!");
+})
